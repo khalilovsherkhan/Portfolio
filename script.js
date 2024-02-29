9f102c5fc7f4db1993201d6214ad722d8010afb1
khalilovsherkhan <xalilovsherxon878@gmail.com>
2024-02-29 13:09:34 +0500

    add

--- script.js
+++ script.js
@@ -4,3 +4,10 @@ window.addEventListener ("scroll", function() {
     header.classList.toggle ("sticky", window.scrollY > 120);
 });
 
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick =  () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('active');
+}
